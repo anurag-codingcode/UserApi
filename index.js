3f4780ed81446454bb61c5c0aa0905a088df201e
Anurag Omprakash <Anurag.Omprakash@godigit.com>
2024-07-23 16:56:37 +0530

    schema implementes the schema for the users

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+const prompt = require('prompt-sync')();
+
+let User=[{
+    email:"anurag123@gmail.com",
+    password:"nfdfnd@039"
+},{
+    email:"rounak4949@gmail.com",
+    password:"94rnfndsfn@jvd"
+}]
+
+
